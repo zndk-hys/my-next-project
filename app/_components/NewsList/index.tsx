d89efad6214297367fc65a322a4f573ce788803f
zndk-hys <hysryt+zndk@gmail.com>
2025-11-17 19:39:50 +0900

    4章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -3,6 +3,7 @@ import styles from './index.module.css';
 import { News } from '@/app/_libs/microcms';
 import Category from '../Category';
 import Date from '../Date';
+import Link from 'next/link';
 
 type Props = {
     news: News[];
@@ -17,23 +18,23 @@ export default function NewsList({ news } : Props) {
     <ul>
         {news.map(article => (
         <li key={article.id} className={styles.list}>
-            <div className={styles.link}>
-            <Image
-                className={styles.image}
-                src="/no-image.png"
-                sizes="200px"
-                alt="No Image"
-                width={1200}
-                height={630}
-            />
-            <dl className={styles.content}>
-                <dt className={styles.title}>{article.title}</dt>
-                <dd className={styles.meta}>
-                    <Category category={article.category} />
-                    <Date date={article.publishedAt ?? article.createdAt} />
-                </dd>
-            </dl>
-            </div>
+            <Link href={`/news/${article.id}`} className={styles.link}>
+                <Image
+                    className={styles.image}
+                    src="/no-image.png"
+                    sizes="200px"
+                    alt="No Image"
+                    width={1200}
+                    height={630}
+                />
+                <dl className={styles.content}>
+                    <dt className={styles.title}>{article.title}</dt>
+                    <dd className={styles.meta}>
+                        <Category category={article.category} />
+                        <Date date={article.publishedAt ?? article.createdAt} />
+                    </dd>
+                </dl>
+            </Link>
         </li>
         ))}
     </ul>
