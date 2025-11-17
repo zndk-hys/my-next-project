b8902c007dc865a93110c55b8b40a2fb0355cd81
zndk-hys <hysryt+zndk@gmail.com>
2025-11-17 20:44:09 +0900

    5章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -19,14 +19,25 @@ export default function NewsList({ news } : Props) {
         {news.map(article => (
         <li key={article.id} className={styles.list}>
             <Link href={`/news/${article.id}`} className={styles.link}>
-                <Image
-                    className={styles.image}
-                    src="/no-image.png"
-                    sizes="200px"
-                    alt="No Image"
-                    width={1200}
-                    height={630}
-                />
+                {article.thumbnail ? (
+                    <Image
+                        className={styles.image}
+                        src={article.thumbnail.url}
+                        sizes="200px"
+                        alt="No Image"
+                        width={1200}
+                        height={630}
+                    />
+                ): (
+                    <Image
+                        className={styles.image}
+                        src="/no-image.png"
+                        sizes="200px"
+                        alt="No Image"
+                        width={1200}
+                        height={630}
+                    />
+                )}
                 <dl className={styles.content}>
                     <dt className={styles.title}>{article.title}</dt>
                     <dd className={styles.meta}>
--- app/page.tsx
+++ app/page.tsx
@@ -2,42 +2,13 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import ButtonLink from './_components/ButtonLink';
 import NewsList from './_components/NewsList';
-import { News } from './_libs/microcms';
+import { getNewsList, News } from './_libs/microcms';
+import { TOP_NEWS_LIMIT } from './_constants';
 
-const data: { contents: News[] } = {
-  contents: [
-    {
-      id: "1",
-      title: "渋谷オフィスを移転しました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19",
-    },
-    {
-      id: "2",
-      title: "当社CEOが業界リーダーTOP30に選出されました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19",
-    },
-    {
-      id: "3",
-      title: "テストの記事です",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/04/19",
-      createdAt: "2023/04/19",
-    },
-  ]
-}
-
-export default function Home() {
-  const sliceData = data.contents.slice(0, 2);
+export default async function Home() {
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  })
   
   return (
     <>
@@ -57,7 +28,7 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <NewsList news={sliceData} />
+        <NewsList news={data.contents} />
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっとみる</ButtonLink>
         </div>
